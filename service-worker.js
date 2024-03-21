0950c03baf2c30751a1103a5f88e0cb33042de89
Akinduko AkinOluwa <116984506+DukeAkinOluwa@users.noreply.github.com>
2024-03-21 13:12:18 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       return cache.addAll([
-        './assets/',
-        './Images/',
+        './edvantage-vite/assets/',
+        './edvantage-vite/Images/',
         'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;500&display=swap'
       ]);
     })
